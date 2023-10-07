95e0d9e5d32399d3fc01d54381e0800983ee64bb
YoussefTamerr <yousseftamer1990@gmail.com>
2023-10-07 21:42:31 +0300

    added client routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,32 @@
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+
 import AdminSignup from "./components/AdminSignup";
 import PharmacistSignup from "./components/PharmacistSignup";
 import PatientSignup from "./components/PatientSignup";
 
 function App() {
   return (
-    <>
-      <PatientSignup />
-    </>
+    <BrowserRouter>
+        <Routes>
+            <Route 
+              path="/" 
+              element={<Navigate to="/signup/patient" />} 
+            />
+            <Route 
+              path="/signup/admin" 
+              element={<AdminSignup />} 
+            />
+            <Route 
+              path="/signup/pharmacist" 
+              element={<PharmacistSignup />} 
+            />
+            <Route 
+              path="/signup/patient" 
+              element={<PatientSignup />} 
+            />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
