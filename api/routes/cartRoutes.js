27698278284a3d4fd1db49e12ea5d99a523820c5
ahmedwbebo <enahmed792@gmail.com>
2023-11-08 13:19:08 +0200

    Cart Routes

---
+++ api/routes/cartRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import {addToCart, getCart, removeFromCart, updateCartItem} from "../controllers/cartController";
+
+const router = express.Router();
+
+router.post('/:add', addToCart);
+
+router.get('/:patientId', getCart);
+
+router.delete('/:id/delete', removeFromCart);
+
+router.batch('/:id/update', updateCartItem);
+
+export default router;
